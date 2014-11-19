7aa21a5e334302378929e66826be7745aabb7705
Patrick Kang <jykang1@gmail.com>
2014-11-19 18:42:31 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,8 +7,8 @@
 - (void)initialize
 {
     // Create a character and add it to the screen.
-    character = (Character*)[CCBReader load:@"Character"];
-    [physicsNode addChild:character];
+    character = (Character*)[CCBReader load:@"Character"]; // loads a new Character from the CCB file named Character. CCB files are created by SpriteBuilder and describe the properties and appearance of in-game objects like our flappy bird.
+    [physicsNode addChild:character]; // add your character as a child of physicsNode so that physics will be applied to it.
 }
 
 -(void)update:(CCTime)delta
@@ -17,5 +17,9 @@
 }
 
 // put new methods here
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
